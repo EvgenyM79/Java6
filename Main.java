43fdb9bcbefe21abfa9911b4cf1b9f85983b5754
EvgenyM79 <56968324+EvgenyM79@users.noreply.github.com>
2019-12-27 19:41:27 +0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-import javax.imageio.plugins.jpeg.JPEGQTable;
+﻿import javax.imageio.plugins.jpeg.JPEGQTable;
 import java.io.IOException;
 import java.util.List;
 
@@ -17,11 +17,11 @@ public class Main {
         }
         f1.writeToFile("lucky","new message");
         FunctionLogging f2 = new FunctionLogging();
-        //Вывод информации в разрезе каждого дня (например на 22.12.2019);
+        //Вывод информации в разрезе каждого дня (например на 27.12.2019);
         f2.readFromFile("27-12-2019.log");
         f2.printLogDay("27-12-2019.log");
         //System.out.println(f2.getLinesLog());
-        //Вывод информации в разрезе пользователя (например на 22.12.2019);
+        //Вывод информации в разрезе пользователя (например на 27.12.2019);
         f2.printLogUser("27-12-2019.log","lucky");
     }
 }
